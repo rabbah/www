95192b0b41d327b4aeddb16f21e482e0e5a4ff28
Rodric Rabbah <rodric@gmail.com>
2018-05-06 17:45:14 -0400

    Detect mobile browser.

--- src/assemble.js
+++ src/assemble.js
@@ -1,7 +1,21 @@
 let common = require('./common')
 
-module.exports.mobile = function(tabs, activeTab, content) {
+module.exports.generator = function(event) {
+  let userAgent = ''
+  if (event.hasOwnProperty('params') && event.params.hasOwnProperty('header') && event.params.header.hasOwnProperty('User-Agent')) {
+    userAgent = event.params.header["User-Agent"]
+  }
+
+  let mobile = ((userAgent.match("iPhone") || userAgent.match("Android"))) != null
+  return (t, a, c) => assemble(t, a, c, mobile)
+}
+
+function assemble(tabs, activeTab, content, mobile) {
   let title = activeTab.title || activeTab.label
+  let bodyClass = mobile ?
+      "no-touch no-header-page wsite-menu-slideright  wsite-theme-light  wsite-page-index wsite-mobile wsite-render3d" :
+      "short-header-page  wsite-theme-light  wsite-page-software-and-artifacts"
+
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -9,17 +23,42 @@ module.exports.mobile = function(tabs, activeTab, content) {
       <title>${title}</title>
       <meta property="og:title" content="${title}"/>
       <meta property="og:description" content="${title}"/>
+      <meta property="og:url" content="https://rabbah.io"/>
+      <meta property="og:site_name" content="Rodric Rabbah"/>
+
       ${common.commonHeaderPrologue()}
-      ${common.mobileHeaderBody()}
-      ${common.commonHeaderEpilogue()}
+      ${mobile ? common.mobileHeaderBody() : common.webHeaderBody()}
+      ${common.commonHeaderEpilogue({mobile: true})}
     </head>
-     
-    <body class="no-touch no-header-page wsite-menu-slideright  wsite-theme-light  wsite-page-index wsite-mobile wsite-render3d" ontouchstart="">
-    ${common.mobileBodyBanner()}
-    ${common.mobileMenu(tabs, activeTab)}
-    ${content}
-    ${common.footer(true)}
-    ${common.gtracker()}
+
+    <body class="${bodyClass}">
+      ${mobile ? common.mobileMenu(tabs, activeTab) : common.webMenu(tabs, activeTab)}
+      ${mobile ? common.mobileBodyBanner() : common.webBodyBanner()}
+
+      ${mobile ? mobileContent(content) : webContent(content)}
+
+      ${common.footer(mobile)}
+      ${common.gtracker()}
     </body>
     </html>`
+}
+
+function mobileContent(content) {
+  return `
+    <div class="content-wrapper">
+      ${content}
+    </div>`
+}
+
+function webContent(content) {
+  return `
+    <div id="main-wrap">
+      <div id="page">
+        <div id="main">
+          <div id="content">
+            ${content}
+          </div>
+        </div>
+      </div>
+    </div>`
 }

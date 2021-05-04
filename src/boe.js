bcd7c580dda800ad1d48462c4c569d6aaa6c1f84
Rodric Rabbah <rodric@gmail.com>
2021-05-03 22:23:40 -0400

    Update boe.

--- src/boe.js
+++ src/boe.js
@@ -23,46 +23,52 @@ function content(mobile) {
         <div class="paragraph">
           <img src="https://rabbah.io/uploads/r.png" alt="Portrait" style="margin: ${mobile? "-10px 20px 20px 20px" : "-10px 20px 20px 20px"}; width: ${mobile? "25%":"20%"}; float: right;border-radius: 50%">
           <span style="color:#3f3f3fe8 !important;font-weight:bold">I am Dr. Rodric Rabbah and I am running for a 3-year term on the Somers Board of Education.</span>
-          My wife, a Cancer Research Scientist at a local hospital, and I have been Somers residents since 2013. We have one child in Kindergarten and another in the third grade. Like many of you during this pandemic, we are doing our best to facilitate our children's learning by day. The pandemic is stressful and heartbreaking for many, so I am finding special comfort in cherishing this unique time with my children.<br/>
+          I have two children in the district, Michel in the fourth grade and Timothy in the first grade. My wife Cynthia is a radiation physicist in cancer treatment.
+          We moved to Somers 8 years ago and we love living here.<br/>
+          <br/>
+          My educational background includes a Doctorate in Computer Science from Georgia Tech, and several more years in academia at MIT.
+          There, I enjoyed teaching new computer science classes to undergraduate and graduate students.<br/>
+          <br/>
+          I then spent eleven years at IBM Research in Yorktown. And now I am the co-founder of a cloud computing company with notable recognition from industry analysts.<br/>
+          <br/>
         </div>
 
         <div class="paragraph">
-          <span style="color:#3f3f3fe8 !important;font-weight:bold">I will bring a fresh perspective to the Board of Education, especially concerning STEAM education (Science, Technology, Engineering, the Arts and Mathematics).</span>
-          I hold a doctorate in Computer Science from Georgia Tech. I was a Research Scientist and taught courses at the Massachusetts Institute of Technology (MIT), helping to connect students with real-world technology. One of my courses was translated and offered online by MIT to a global audience. I was a Principal Scientist at IBM Research where I spent 11 years of my career. Two years ago, I decided to leave IBM to start my own technology company.<br/>
+          <span style="color:#3f3f3fe8 !important;font-weight:bold">My academic roots, and my experience with research and in industry make me uniquely qualified to be your Trustee on the Somers Board of Education.</span> My top three reasons for running in this election:
+          <ol>
+            <li>To ensure our district is preparing every student for a future that is going to heavily emphasize STEAM.</li>
+            <li>Advocacy for the elementary grades and Individualized Education Programs for children with special needs.</li>
+            <li> Fiscal responsibility and strategic investment in our educators and facilities.</li>
+          </ol>
+          We need new perspectives and relevant experience on the Board, and I will be an effective advocate for you and your children.<br/>
+          <br/>
         </div>
 
         <div class="paragraph">
-          <span style="color:#3f3f3fe8 !important;font-weight:bold">The pandemic has brought about new challenges and realities around distance learning for which I have real world expertise.</span>
-          I found it both surprising and disturbing to learn recently that some members of the Board of Education did not realize how many parents found the rollout of the distance learning program to be frustratingly slow.<br/>
+          <span style="color:#3f3f3fe8 !important;font-weight:bold">Highlights of my involvment with the District and Administration in the past year:</span>
+          <ul>
+            <li>I have engaged our administration to discuss ways to make computer science instruction more practical given industry trends.</li>
+            <li>I joined the PTA Council and serve as your PTA delegate for Primrose.</li>
+            <li>I take part of the Superintendent’s Educational Equity Advisory Team (SEEAT).</li>
+            <li>I participate in the SEEAT curriculum reviews with our district educators.</li>
+            <li>I attend monthly round tables with our superintendent.</li>
+            <li>I attend and participate in Board meetings regularly.</li>
+            <li>I have consulted with the administration on issues impacting e-learning.</li>
+          </ul>
         </div>
 
         <div class="paragraph">
-          <span style="color:#3f3f3fe8 !important;font-weight:bold">I know that establishing distance learning programs requires unique technologies and integrations, for which I can offer first-hand experience based on running my fully remote company.</span>
-          I am very familiar with the technologies being used and evaluated by the district, and I understand the pros and cons of these online tools, including their privacy implications. I also understand that distance learning for younger children is not the same as distance learning for older children. A comprehensive distance and remote learning plan must include how to best support and integrate our district's excellent Individualized Education Programs for children with special needs, and must address the social and emotional well-being of our students.<br/>
-        </div>
-
-        <div class="paragraph">
-          <span style="color:#3f3f3fe8 !important;font-weight:bold">I am keenly interested in how our district is approaching STEAM readiness, and the curriculum changes that are being considered by the Superintendent.</span>
-          While Somers is a high-ranking academic district both in Westchester and nationally, I believe we could do better. I often speak with other parents, many of whom are educators, for their perspectives. What I have found is a consistent reinforcement of my belief. I support our district's approach of focusing on the child as a whole, but I also think we can be more tactical in our approach to preparing our students for the future.<br/>
-        </div>
-
-        <div class="paragraph">
-          <span style="color:#3f3f3fe8 !important;font-weight:bold">Of course changes take time, training and investment, and I am already committed for the entire journey.</span>
-          Earlier this year, I joined several committees organized by the Superintendent to raise awareness about the need for better communication with parents.
-        </div>
-
-        <div class="paragraph">
-          <span style="color:#3f3f3fe8 !important;font-weight:bold">In summary, my background, my unique perspectives, and my experience would be vital assets in addressing the challenges facing the district. I hope I have earned your vote for the privilege to represent you and your children on the School Board.</span>
+          <span style="color:#3f3f3fe8 !important;font-weight:bold">In summary, my background, my unique perspectives, and my experience would be vital assets to help push our district forward to equip everyone of our students for the 21st century. This starts in the elementary grades, with a strong emphasis on STEAM, and with responsible investment in our educators and facilities.</span><br/>
           <br/>
         </div>
 
         <div id="info" class="paragraph">
           Please share this letter with your friends and family.<br/>
           <ul>
-            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">Your ballot must be received by 5PM on June 9:</span> the election this year will only be held by mail-in ballots.</li>
-            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">To receive a paper ballot if one was not mailed to you by June 2:</span> email our District Clerk Nancy Corrado at <a href='mailto:ncorrado@somersschools.org'>ncorrado@somersschools.org</a>.</li>
-            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">If you are not registered to vote:</span> visit <a href='https://dmv.ny.gov/more-info/electronic-voter-registration-application'> https://dmv.ny.gov/more-info/electronic-voter-registration-application</a> to register online.</li>
-            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">To contact me:</span> email me at <a href='mailto:rodric@gmail.com'>rodric@gmail.com</a> or call <a href='tel:+16462486456'>646-248-6456</a>.</li>
+            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">Vote on May 18 at the Somers Middle School. Polls are open from 7am to 9pm.</li>
+            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">To receive a paper ballot, you must request one no later than May 11. Email our District Clerk Nancy Corrado at <a href='mailto:ncorrado@somersschools.org'>ncorrado@somersschools.org</a>.</li>
+            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">If you are not registered to vote, visit <a href='https://dmv.ny.gov/more-info/electronic-voter-registration-application'> https://dmv.ny.gov/more-info/electronic-voter-registration-application</a> to register online.</li>
+            <li><span style="color:#3f3f3fe8 !important;font-weight:bold">Contact me at <a href='mailto:rodric@gmail.com'>rodric@gmail.com</a> or call <a href='tel:+16462486456'>646-248-6456</a>.</li>
           </ul>
           <br/>
           Respectfully,<br/>
